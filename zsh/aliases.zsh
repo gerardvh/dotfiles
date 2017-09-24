511ed83226791348a507821bd3f1d7dfbccbb3b3
Gerard Van Halsema <gerardvh@me.com>
2017-09-23 22:42:50 -0500

    Add more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias get='curl -OL'
+alias g='grep -i'
+alias ll="ls -lahL"
+alias con="tail -40 -f /var/log/system.log"
+alias edit='subl -w'
