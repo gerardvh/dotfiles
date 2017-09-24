1784141cb6648a8b0c6a267a63541bcb8320fe33
Gerard Van Halsema <gerardvh@me.com>
2017-09-23 22:08:37 -0500

    SSH Key setup

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,5 @@
+# Specify which ssh key to use
+export SSH_KEY_PATH="~/.ssh/id_rsa"
+
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
