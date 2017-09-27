026e2707cbd6c0187ab8a553c51dbb2e82f6c0f2
Gerard Van Halsema <gerardvh@me.com>
2017-09-27 10:57:51 -0500

    WIP

--- fish-shell/install.fish
+++ fish-shell/install.fish
@@ -1,23 +1,42 @@
+#!/usr/bin/env fish
+
+set -l FISH_DIR (dirname (status --current-filename))
+# echo "This directory: $FISH_DIR"
+
 # Add fish folder if it doesn't exist
 mkdir -p ~/.config/fish
 
 # Symlink everything in the fish sibling-folder with the '.symlink' ext.
+for file in (find -H $FISH_DIR -maxdepth 2 -name "*.symlink")
+  set -l dest_name (string replace '.symlink' '' (basename $file))
+  set -l full_src (realpath $file)
+  set -l full_dest (realpath $fish_config)"/$dest_name"
 
+  # Need special behavior if the existing destination is a directory
+  if test -d $full_dest
+    echo "Backing up existing directory:"
+    echo "$full_dest ---> $full_dest.backup"
+    mv -f $full_dest "$full_dest.backup"
+  end
+  echo "$full_src ------> $full_dest"
+  ln -sF $full_src $full_dest
+end
 
 
 # Install fisherman from github ()
 set -l fisherman ~/.config/fish/functions/fisher.fish
 if test -e $fisherman
   # Alredy have fisherman installed
-  echo 'Fisherman is already installed. Remove $fisherman before re-installing.'
+  echo "Fisherman is already installed. No need to install."
+  echo "Remove $fisherman if you would like to reinstall."
 else
   # Don't have fisherman installed, install it
-  # curl -Lo $fisherman --create-dirs https://git.io.fisher
   echo "About to install Fisherman to: $fisherman"
+  # curl -Lo $fisherman --create-dirs https://git.io/fisher
 end
 
 # Link fishfile
-link -sF $DOTS/fish/fishfile ~/.config/fish/fishfile
+# ln -sF $DOTS/fish-shell/fish/fishfile.symlink ~/.config/fish/fishfile
 
 # Run fisherman install
-fisher
+# fisher
