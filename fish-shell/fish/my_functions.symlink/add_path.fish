652bf6af529ebd441a3b41d4a1aaa858418e6766
Gerard Van Halsema <gerardvh@me.com>
2017-09-28 22:33:31 -0500

    Remove echos

--- fish-shell/fish/my_functions.symlink/add_path.fish
+++ fish-shell/fish/my_functions.symlink/add_path.fish
@@ -1,15 +1,8 @@
-function add_path --description 'Add an individual path to fish_user_paths, if not there already.'
+function add_path \
+    --description 'Add an individual path to fish_user_paths, if not there already.'
 	for path in $argv
     if not contains $path $fish_user_paths
       set -U fish_user_paths $fish_user_paths $path
-      if isatty
-        echo Updating fish_user_paths with: $path
-        echo New PATH is: $PATH
-      end
-    else
-      if isatty
-        echo Entry already exists in fish_user_paths: $path
-      end
     end
   end
 end
