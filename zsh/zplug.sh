5a495d3448b7516c282a477214e356b34c198e58
Gerard Van Halsema <gerardvh@me.com>
2017-10-02 10:14:04 -0500

    Find all zplugs and load them

--- zsh/zplug.sh
+++ zsh/zplug.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+# Find all the 'zplugs.sh' files and source them for relevant plugins
+for plug_file ($DOTFILES/**/zplugs.sh) source $plug_file
 
+# Install plugins if there are plugins that have not been installed
+if ! zplug check; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+# Load plugins
+zplug load
