924a775ff711182f905659041e168e10ee58f7bd
Gerard Van Halsema <gerardvh@me.com>
2017-10-02 10:36:21 -0500

    Add useful zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,35 @@
-alias reload!='. ~/.zshrc'
-
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias get='curl -OL'
 alias g='grep -i'
 alias con="tail -40 -f /var/log/system.log"
 alias edit='subl -w'
+
+# reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+    colorflag="--color"
+else # OS X `ls`
+    colorflag="-G"
+fi
+
+alias vim="nvim"
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
